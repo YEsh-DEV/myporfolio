f1b5403e244e409bc9ea36e4b5f662a8f4958f04
Yeshwanth Atmakuri <atmakuriyeshwanth@gmail.com>
2025-02-12 22:24:08 +0530

    Update configuration for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/yeshportfolio",
+  base: '/myporfolio/',
   plugins: [react()],
   assetsInclude: ['**/*.gltf'],
   server: {
